18da8c658c7265db9f8623353818f9d837970ca9
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-08 03:59:44 +0100

    Import navLayer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.sass";
+import Nav from "./Components/Nav/Nav";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import { useState } from "react";
+import NavLayer from "./Components/Nav/NavLayer";
 
 function App() {
+	const [navIsExpanded, setNavIsExpanded] = useState(false);
 	return (
 		<div className="App">
 			<BrowserRouter>
+				<Nav
+					navIsExpanded={navIsExpanded}
+					setNavIsExpanded={setNavIsExpanded}
+				/>
+				<NavLayer navIsExpanded={navIsExpanded} />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/products" element={<Products />} />
