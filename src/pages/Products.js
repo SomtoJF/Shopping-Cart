088b12dcbd629cf1f02eccd3125ee05424c7a2c1
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-13 16:21:33 +0100

    Import Items Component

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,18 @@
 import ProductsHeader from "../Components/Header";
+import Item from "../Components/products/Item";
 import "../styles/products/Products.sass";
 
 const Products = ({ products }) => {
-	return <ProductsHeader text="MINIMALIST COLLECTIONS" />;
+	return (
+		<div id="products-page">
+			<ProductsHeader text="MINIMALIST COLLECTIONS" />
+			<ol id="products">
+				{products.map((item) => (
+					<Item product={item} key={item.sn} />
+				))}
+			</ol>
+		</div>
+	);
 };
 
 export default Products;
