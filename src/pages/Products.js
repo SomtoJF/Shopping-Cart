25362eb59219497f713bc9a0126370561a28a072
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-13 15:18:19 +0100

    refactor: Edit props as Header has been made reuseable

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,8 @@
-import ProductsHeader from "../Components/products/Header";
+import ProductsHeader from "../Components/Header";
 import "../styles/products/Products.sass";
 
 const Products = ({ products }) => {
-	return <ProductsHeader />;
+	return <ProductsHeader text="MINIMALIST COLLECTIONS" />;
 };
 
 export default Products;
