a596958754287b254a825aa15cf2e3e510d2f326
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-13 19:23:41 +0100

    Refactor: Make code more readable by mapping outside return statement

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,14 +3,12 @@ import Item from "../Components/products/Item";
 import "../styles/products/Products.sass";
 
 const Products = ({ products }) => {
+	const items = products.map((item) => <Item product={item} key={item.sn} />);
+
 	return (
 		<div id="products-page">
 			<ProductsHeader text="MINIMALIST COLLECTIONS" />
-			<ol id="products">
-				{products.map((item) => (
-					<Item product={item} key={item.sn} />
-				))}
-			</ol>
+			<ol id="products">{items}</ol>
 		</div>
 	);
 };
