940213bd475592fd3140ad26d0376e96b644ad97
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-22 15:51:14 +0100

    Initial Build of product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchAProduct } from "../API/FetchProducts";
+import Item from "../Components/products/Item";
+import "../styles/product/product.sass";
 
 const Product = () => {
 	const { id } = useParams();
@@ -8,9 +10,14 @@ const Product = () => {
 	useEffect(() => {
 		fetchAProduct(id).then((response) => {
 			setProduct(response);
+			console.log(response);
 		});
 	}, []);
-	return <h1>hello {product.photographer}</h1>;
+	return (
+		<div id="product-page">
+			<Item product={product} breakdown={false} />
+		</div>
+	);
 };
 
 export default Product;
