de2bbbcd1909c00c605228a8082e6992f5de1123
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-22 15:45:10 +0100

    Rebuild Item Component to ensure reusability

--- src/Components/products/Item.js
+++ src/Components/products/Item.js
@@ -3,6 +3,18 @@ import "../../styles/products/Item.sass";
 
 const Item = (props) => {
 	const product = props.product;
+	const breakdown = props.breakdown;
+	if (breakdown === true) {
+		return <BrokenDownItem item={product} />;
+	} else {
+		return <ExpandedItem item={product} />;
+	}
+};
+
+export default Item;
+
+const BrokenDownItem = (props) => {
+	const product = props.item;
 	return (
 		<li className="product-item">
 			<img src={product.src} alt={product.alt} />
@@ -30,4 +42,45 @@ const Item = (props) => {
 	);
 };
 
-export default Item;
+const ExpandedItem = (props) => {
+	const product = props.item;
+	return (
+		<main id="a-product">
+			<img src={product.src} alt={product.alt} />
+			<section>
+				<ul>
+					<li>
+						<div>Product</div>
+						<div>{product.alt}</div>
+					</li>
+					<li>
+						<div>Photographer</div>
+						<a href={product.photographer_url}>{product.photographer}</a>
+					</li>
+
+					<li>
+						<div>Price</div>
+						<div>${product.price}</div>
+					</li>
+					<li>
+						<div>Poster Size</div>
+						<div>{product.poster_size} cm</div>
+					</li>
+					<li>
+						<div>Color</div>
+						<div
+							style={{
+								backgroundColor: `${product.color}`,
+							}}
+						>
+							{product.color}
+						</div>
+					</li>
+					<li>
+						<button>Add to Cart</button>
+					</li>
+				</ul>
+			</section>
+		</main>
+	);
+};
