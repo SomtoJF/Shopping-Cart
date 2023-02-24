145c85d2928c3b4575e98e0a8f9a4c4c155dbf0c
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-24 15:39:31 +0100

    Build Component logic for cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,32 @@
-const Cart = () => {
-	return <h1>Hello from Cart</h1>;
+import { useEffect, useState } from "react";
+import CartItem from "../Components/cart/CartItem";
+import "../styles/cart/Cart.sass";
+
+const Cart = ({ setCart, cart }) => {
+	const [paymentAmount, setPaymentAmount] = useState(0);
+	useEffect(() => {
+		setPaymentAmount(cart.reduce(getAmount, 0));
+		function getAmount(total, item) {
+			return total + item.price * item.quantity;
+		}
+	}, [cart]);
+	return (
+		<div id="cart-page">
+			<h2>
+				<div>PAYMENT AMOUNT:</div>
+				<div>${paymentAmount}</div>
+			</h2>
+
+			{cart.map((item, index) => (
+				<CartItem
+					product={item}
+					setCart={setCart}
+					cart={cart}
+					key={index + 100000}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default Cart;
